860a6b963f8302424711318451d38566469feb4b
Wutthichai Panyasung <wutibank@gmail.com>
2018-05-13 13:17:23 +0700

    Edit ui activity card

--- resources/views/layouts/activity.blade.php
+++ resources/views/layouts/activity.blade.php
@@ -15,7 +15,31 @@
 <section class="section" id="section-activitylist">
     <div class="container">
 
-        <div class="columns is-multiline">
+            <div class="columns is-multiline is-centered tileAmimate">
+                    @foreach ($act as $item)
+                <div class="example-1 column is-12-mobile is-6-tablet is-4-desktop card-item">
+                    <div class="wrapper" style="background: url({{asset('storage/image/activities')}}/{{$item->cover}}) 20% 1%/cover no-repeat;background-color:#222">
+                        <div class="date" style="background-color: #f25757;">
+                            <span class="day">{{ Carbon\Carbon::parse($item->created_at)->format('d')}}</span>
+                            <span class="month">{{ Carbon\Carbon::parse($item->created_at)->format('M')}}</span>
+                            <span class="year">{{ Carbon\Carbon::parse($item->created_at)->format('Y') + 543}}</span>
+                    </div>
+                    <div class="data">
+                        <div class="content">
+                            <span class="author">{{ $item->organizer }}</span>
+                            <h1 class="title is-size-5-mobile is-size-3-desktop"><a href="{{url('activity/'.$item->id)}}">{{ $item->title }}</a></h1>
+                            <p class="text">{{ $item->subtitle }}</p>
+                            <span></span>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                @endforeach
+    
+            </div>
+
+
+        {{-- <div class="columns is-multiline">
             @foreach ($act->shuffle() as $acti)
             <div class="column is-4">
                 <div class="card">
@@ -42,7 +66,7 @@
             </div>
             @endforeach
 
-        </div>
+        </div> --}}
 
     </div>
 </section>
